2d4316164e9f83dd1753a197d09130a184a5697d
angeloscode <itsxlfs@gmail.com>
2026-06-23 10:47:16 +0300

    бдшка

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",     // или ./src/lib/db/schema.ts
+  schema: "./src/db/schema.ts",
   out: "./drizzle",
-  dialect: "sqlite",
+  dialect: "postgresql",
   dbCredentials: {
-    url: "./sqlite.db",             // файл базы будет создан здесь
+    url: process.env.POSTGRES_URL!,
   },
 });
